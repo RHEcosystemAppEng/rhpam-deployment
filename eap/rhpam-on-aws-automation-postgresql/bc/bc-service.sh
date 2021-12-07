19e4cedc586a986a7a42a7be0df45524d73f6e6c
Zvi Grinberg <zgrinber@redhat.com>
2021-12-07 16:02:12 +0200

    initial load of business-central automation script

---
+++ eap/rhpam-on-aws-automation-postgresql/bc/bc-service.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+source /opt/custom-config/business-central.properties
+
+${EAP_HOME}/bin/standalone.sh -c standalone-full.xml -b 0.0.0.0 \
+-Drhpam.sso.token=${SSO_BC_SECRET} -Drhpam.sso.auth_url=${SSO_AUTH_URL} \
+-Dbusiness.central.client.secret=${SSO_KS_SECRET} \
+-Dorg.kie.server.user=${CONTROLLER_USERNAME} \
+-Dorg.kie.server.pwd=${CONTROLLER_PASSWORD} 
