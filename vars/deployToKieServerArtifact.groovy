a41271e9de227652dbb421b4d2cc5fcc48508dd4
zvigrinberg <zgrinberg@gmail.com>
2021-12-15 16:51:50 +0200

    fix url for deployment service

--- vars/deployToKieServerArtifact.groovy
+++ vars/deployToKieServerArtifact.groovy
@@ -3,7 +3,7 @@ package org.demo
 def call(String server,String userName,String password,String containerId, String groupId , String artifactId, String version )
 {
     echo "started deployToKieServerArtifact/7"
-    def contextUrl = "/kie-server/rest/server/containers/"
+    def contextUrl = "kie-server/services/rest/server/containers/"
     def url = "http://" + server + contextUrl + containerId
     def userPass = userName + ":" + password;
     def basicAuthBase64 = sh(script: "echo -n $userPass | base64",returnStdout: true ).trim()
