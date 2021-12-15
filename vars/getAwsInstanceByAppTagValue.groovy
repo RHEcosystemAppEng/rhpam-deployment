f5213f8033a6bd41cecdde9acd6f1507bba40f51
zvigrinberg <zgrinberg@gmail.com>
2021-12-15 18:27:28 +0200

    try fetching tag according to key instead of name

--- vars/getAwsInstanceByAppTagValue.groovy
+++ vars/getAwsInstanceByAppTagValue.groovy
@@ -2,7 +2,7 @@ def call(String appName)
 {
 
     echo "started getInstanceByAppTagValue/1"
-    def result = sh(script: "aws ec2 describe-tags --filters \"Name=resource-type,Values=instance\" \"Name=app,Values=${appName}\"",returnStdout : true)
+    def result = sh(script: "aws ec2 describe-tags --filters \"Name=resource-type,Values=instance\" \"Key=app,Values=${appName}\"",returnStdout : true)
 
     echo "result is ${result}"
     return result
