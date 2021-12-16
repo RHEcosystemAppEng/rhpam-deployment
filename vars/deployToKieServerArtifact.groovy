fa2bb59b4e0b05b3fea00c689e5d68869090fc8b
zvigrinberg <zgrinberg@gmail.com>
2021-12-16 12:57:24 +0200

    fix security issue 4

--- vars/deployToKieServerArtifact.groovy
+++ vars/deployToKieServerArtifact.groovy
@@ -9,9 +9,9 @@ def call(String server,String userName,String password,String containerId, Strin
     def basicAuthBase64 = sh(script: "echo -n $userPass | base64",returnStdout: true ).trim()
     def authHeader = "Authorization: Basic " + basicAuthBase64
     def result = sh(script: 'curl --location --request PUT $url \
-            --header 'Accept: application/json' \
-            --header 'Content-Type: application/json' \
-            --header '$authHeader' \
+            --header Accept: application/json \
+            --header Content-Type: application/json \
+            --header $authHeader \
              --data '{\"container-id\" : \"$containerId\",\"release-id\" : { \"group-id\" : \"$groupId\",\"artifact-id\" : \"$artifactId\",\"version\" : \"$version\" }}'',returnStdout : true)
 
     echo "result is ${result}"
