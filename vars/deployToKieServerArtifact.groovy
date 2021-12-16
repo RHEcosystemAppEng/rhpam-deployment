3a77dc650a8e63dfc32f18342012c705dc8fa7f4
zvigrinberg <zgrinberg@gmail.com>
2021-12-16 12:44:42 +0200

    fix security issue

--- vars/deployToKieServerArtifact.groovy
+++ vars/deployToKieServerArtifact.groovy
@@ -12,7 +12,7 @@ def call(String server,String userName,String password,String containerId, Strin
     def result = sh(script: "curl --location --request PUT ${url} \
             --header 'Accept: application/json' \
             --header 'Content-Type: application/json' \
-            --header '${authHeader}' \
+            --header '$authHeader' \
              --data '{\"container-id\" : \"${containerId}\",\"release-id\" : { \"group-id\" : \"${groupId}\",\"artifact-id\" : \"${artifactId}\",\"version\" : \"${version}\" }}'",returnStdout : true)
 
     echo "result is ${result}"
