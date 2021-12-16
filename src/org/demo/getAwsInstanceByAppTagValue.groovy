802faa154f7114e8c667a60b7c7984af20b29f98
zvigrinberg <zgrinberg@gmail.com>
2021-12-16 12:32:31 +0200

    adjust pipeline functions

--- src/org/demo/getAwsInstanceByAppTagValue.groovy
+++
@@ -1,9 +0,0 @@
-def call(String appName)
-{
-
-    echo "started getInstanceByAppTagValue/1"
-    def result = sh(script: "aws ec2 describe-tags --filters \"Name=resource-type,Values=instance\" \"Name=app,Values=${appName}\"",returnStdout : true)
-
-    echo "result is ${result}"
-    return result
-}
