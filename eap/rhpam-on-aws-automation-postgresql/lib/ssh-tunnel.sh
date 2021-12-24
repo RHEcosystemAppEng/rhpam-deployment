0c8c76d0bbc905fe449d6d29c51fd1cbc121b103
Daniele Martinoli <dmartino@redhat.com>
2021-12-24 12:30:54 +0100

    https://issues.redhat.com/browse/APPENG-206 Support SSH tunnel Reviewd options of ssh-tunnel script: added tunnel host, removed sudo user and remote port (just use 22)

--- eap/rhpam-on-aws-automation-postgresql/lib/ssh-tunnel.sh
+++ eap/rhpam-on-aws-automation-postgresql/lib/ssh-tunnel.sh
@@ -1,55 +1,50 @@
 #!/bin/bash
 
 function usage() {
-  echo "Usage: $0 [-h] [-k] -l <local port> [-s <SSH pem file>] [-u <SSH user>] [-t <target host/IP>] [-p <target port>] [-n]"
+  echo "Usage: $0 [-h] [-k] -l <local port> [-s <SSH pem file>] [-u <SSH user>] [-t <tunnel host>] [-r <remote host/IP>]"
   echo "   -h to print this message"
   echo "   -k to kill an existing SSH tunnel"
-  echo "   -n to use current user instead of sudo (no-sudo)"
   exit 1;
 }
 
 function killTunnel() {
-  [[ "${useSudo}" = true ]] && cmd="sudo kill" || cmd="kill"
-  echo "Deleting SSH tunnel at localhost:${localPort}. Command is ${cmd}"
-  ps -ef | grep "ssh -i.*${localPort}" | grep -v grep | awk '{print $2}' | xargs $cmd
+  echo "Deleting SSH tunnel at localhost:${localPort}"
+  ps -ef | grep "ssh -i.*${localPort}" | grep -v grep | awk '{print $2}' | xargs sudo kill
 }
 
 function cleanKnownHosts(){
-  [[ "${useSudo}" = true ]] && cmd="sudo sed" || cmd="sed"
-  echo "Removing entry from ~/.ssh/known_hosts for localhost:${localPort}. Command is ${cmd}"
-  ${cmd} -i -e "/^\[localhost\]:${localPort} .*$/d" ~/.ssh/known_hosts
+  echo "Removing entry from ~/.ssh/known_hosts for localhost:${localPort}"
+  sudo sed -i -e "/^\[localhost\]:${localPort} .*$/d" ~/.ssh/known_hosts
 }
 
 function createSSHTunnel(){
-    [[ "${useSudo}" = true ]] && cmd="sudo ssh" || cmd="ssh"
-    echo "Creating SSH tunnel for ${sshUser} using ${sshPemFile} to ${remoteHost}:${remotePort} on localhost:${localPort}. Command is ${cmd}"
-    ${cmd} -i ${sshPemFile} ${sshUser}@18.197.92.7 -N -f  -L ${localPort}:${remoteHost}:${remotePort}
+  echo "Creating SSH tunnel through ${tunnelHost} for ${sshUser}/${sshPemFile} to ${remoteHost}:${remotePort} on localhost:${localPort}"
+  sudo ssh -i ${sshPemFile} ${sshUser}@${tunnelHost} -N -f -L ${localPort}:${remoteHost}:${remotePort}
 }
 
 option='create'
 unset localPort
 unset sshPemFile
 unset sshUser
+unset tunnelHost
 unset remoteHost
 remotePort=22
-useSudo=true
-while getopts "hkl:s:u:t:p:n" o; do
+while getopts "hkl:s:u:t:r:" o; do
   case "${o}" in
     k)  option='kill';;
     l)  localPort=${OPTARG};;
     s)  sshPemFile=${OPTARG};;
     u)  sshUser=${OPTARG};;
-    t)  remoteHost=${OPTARG};;
-    p)  remotePort=${OPTARG};;
-    n)  useSudo=false;;
+    t)  tunnelHost=${OPTARG};;
+    r)  remoteHost=${OPTARG};;
     h|?)  echo "Unknown option ${o}"
         usage ;;
   esac
 done
 
-#echo "$option $localPort $sshPemFile $sshUser $remoteHost $remotePort $useSudo"
+#echo "$option $localPort $sshPemFile $sshUser $tunnelHost $remoteHost $remotePort"
 if [ "${option}" == 'create' ]; then
-  if [ -z "${localPort}" ] || [ -z "${sshPemFile}" ] || [ -z "${sshUser}" ] || [ -z "${remoteHost}" ]; then
+  if [ -z "${localPort}" ] || [ -z "${sshPemFile}" ] || [ -z "${sshUser}" ] || [ -z "${tunnelHost}" ] || [ -z "${remoteHost}" ]; then
     echo "Missing expected parameters to create a new SSH tunnel"
     usage
   else
