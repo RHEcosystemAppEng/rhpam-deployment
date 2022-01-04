c807cde15af7d053af4a42d3bc1b42f30eebbc77
zvigrinberg <zgrinberg@gmail.com>
2022-01-04 18:33:41 +0200

    fix aws command - put key values in double quotes

--- vars/getAwsAsgNameByTagAndValue.groovy
+++ vars/getAwsAsgNameByTagAndValue.groovy
@@ -2,7 +2,7 @@ def call(String tagKey,String tagValue)
 {
 
     echo "started getAwsAsgNameByTagAndValue/2"
-    def result = sh(script: "aws autoscaling describe-auto-scaling-groups --filters  Name=tag-key,Values=${tagKey}  Name=tag-value,Values=${tagValue} ",returnStdout : true)
+    def result = sh(script: "aws autoscaling describe-auto-scaling-groups --filters  \"Name=tag-key,Values=${tagKey}  Name=tag-value,Values=${tagValue}\" ",returnStdout : true)
 
     echo "result is ${result}"
     def resultJson = readJSON text : result
