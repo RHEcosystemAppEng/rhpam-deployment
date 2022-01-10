abf878522f853c181f3f750094946465cd2d1917
zvigrinberg <zgrinberg@gmail.com>
2022-01-10 13:43:36 +0200

    few fixes

--- vars/deployToKieServerArtifact.groovy
+++ vars/deployToKieServerArtifact.groovy
@@ -6,13 +6,12 @@ def call(String server,String userName,String password,String containerId, Strin
     def contextUrl = "/kie-server/services/rest/server/containers/"
     def url = "http://" + server + contextUrl + containerId
     def userPass = userName + ":" + password;
-    def basicAuthBase64 = sh(script: "echo -n $userPass | base64",returnStdout: true ).trim()
-    def authHeader = "Authorization: Basic " + basicAuthBase64 
+//    def basicAuthBase64 = sh(script: "echo -n $userPass | base64",returnStdout: true ).trim()
+//    def authHeader = "Authorization: Basic " + basicAuthBase64
 //    echo "result is ${basicAuthBase64}"
-    def result = sh(script: "curl --location --request PUT ${url} \
+    def result = sh(script: "curl --user ${userPass} --location --request PUT ${url} \
             --header 'Accept: application/json' \
             --header 'Content-Type: application/json' \
-            --header '${authHeader}' \
              --data '{\"container-id\" : \"${containerId}\",\"release-id\" : { \"group-id\" : \"${groupId}\",\"artifact-id\" : \"${artifactId}\",\"version\" : \"${version}\" }}'",returnStdout : true)
 
     echo "result is ${result}"
