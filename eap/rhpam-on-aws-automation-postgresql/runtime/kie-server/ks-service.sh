e1909c880ab72e9922679cba616b087198b173b8
Daniele Martinoli <dmartino@redhat.com>
2022-01-14 09:43:44 +0100

    https://issues.redhat.com/browse/APPENG-250 Replaced businessCentral_host and businessCentral_port with a single businessCentral_url property

--- eap/rhpam-on-aws-automation-postgresql/runtime/kie-server/ks-service.sh
+++ eap/rhpam-on-aws-automation-postgresql/runtime/kie-server/ks-service.sh
@@ -34,5 +34,5 @@ ${EAP_HOME}/bin/standalone.sh -c standalone-full.xml -b 0.0.0.0 \
   -DkeycloakSso_deployment=kie-server.war \
   -DkeycloakSso_realm_client_name=${keycloakSso_realm_client_name} -DkeycloakSso_realm_client_secret=${keycloakSso_realm_client_secret} \
   -Dkieserver_privateIp=${kieserver_privateIp} -Dkieserver_hostname=${kieserver_hostname} -Dkieserver_port=${RHPAM_SERVER_PORT} \
-  -DbusinessCentral_host=${businessCentral_host} -DbusinessCentral_port=${businessCentral_port} -DrhpamController_username=${rhpamController_username} -DrhpamController_password=${rhpamController_password} \
+  -DbusinessCentral_url=${businessCentral_url} -DrhpamController_username=${rhpamController_username} -DrhpamController_password=${rhpamController_password} \
   -Drhpam_server_data_dir=${RHPAM_HOME}
