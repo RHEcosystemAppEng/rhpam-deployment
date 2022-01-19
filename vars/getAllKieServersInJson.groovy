f05bc4fda9a50ae50ea947c3a761694335899d03
zvigrinberg <zgrinberg@gmail.com>
2022-01-19 10:26:26 +0200

    related JIRA ticket : https://https://issues.redhat.com/browse/APPENG-259 Description: Change DEV pipeline Deployment of artifacts to be via bc controller REST API

---
+++ vars/getAllKieServersInJson.groovy
@@ -0,0 +1,22 @@
+/**
+ *
+ * @param server - business-central dns/ip address along with :port(example ip_address:8080).
+ * @param userName - user name of controller user with role 'rest-all'
+ * @param password - password of the above username
+ * @return json string containing a json array with all kie-server registered with business-central.
+ */
+def call(String server,String userName,String password)
+{
+
+    echo "started getAllKieServersInJson/3"
+    def contextUrl = "/business-central/rest/controller/management/servers"
+    String url = "http://$server$contextUrl"
+
+    def allKieServers = sh(script: "curl --user ${userName}:${password} -X GET ${url} --header 'Accept: application/json' ",returnStdout : true)
+
+
+    echo "the kie-servers list : \\n  ${allKieServers}"
+
+    return allKieServers
+}
+
